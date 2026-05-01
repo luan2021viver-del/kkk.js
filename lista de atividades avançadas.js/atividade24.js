b33478f644f1eac68742084553c860aef715ffe1
luan2021viver-del <luan2021viver@gmail.com>
2026-04-30 21:13:55 -0300

    Create atividade24.js

---
+++ lista de atividades avançadas.js/atividade24.js
@@ -0,0 +1,8 @@
+function calcularAlturaPredio(alturapredio, sombraPessoa, sombraPredio){
+    return (alturapessoa * sombraPredio)/ sombraPessoa;
+}
+let alturapesoa = parseInt(prompt("Digite a altura da pessoa: "));
+let sombraPredio = parseInt(prompt("Digite o comprimento da sombra do predio: "));
+let sombrapessoa = parseInt(prompt("Digite o comprimento da sombra da pessoa: "));
+let alturapredio = alturapessoa * sombraPredio/ sombrapessoa
+("A altura do predio é",)
