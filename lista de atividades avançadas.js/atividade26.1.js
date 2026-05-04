65f5219efa0c87a80f52439442338df3fc4e6912
luan2021viver-del <luan2021viver@gmail.com>
2026-05-04 19:58:38 -0300

    Create atividade26.1.js

---
+++ lista de atividades avançadas.js/atividade26.1.js
@@ -0,0 +1,24 @@
+/ Função que calcula salário bruto
+function calcularSalarioBruto(horas, valorHora) {
+     salarioBruto = horas * valorHora;
+     return salarioBruto;
+}
+
+// Função que aplica bônus
+function calcularSalarioFinal(salarioBruto) {
+    if (salarioBruto < 2000) {
+        return salarioBruto + 200;
+    }
+    return salarioBruto;
+}
+
+// Programa principal
+let nome = prompt("Digite seu nome: ");
+let horas = Number(prompt("Horas trabalhadas: "));
+let valorHora = Number(prompt("Valor da hora: "));
+
+let salarioBruto = calcularSalarioBruto(horas, valorHora);
+let salarioFinal = calcularSalarioFinal(salarioBruto);
+
+console.log(`Funcionário: ${nome}`);
+console.log(`Salário final: R$ ${salarioFinal.toFixed(2)}`);
